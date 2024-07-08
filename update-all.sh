3e76dcb88c1afd7246a8c97ce79e61fbf6b43848
Aurélien Thévoz <aurelien.thevoz@logi.one>
2024-07-08 13:29:46 +0200

    Add script to update-all automatically

--- update-all.sh
+++ update-all.sh
@@ -1,30 +1,15 @@
-# Update all
+!#/bin/bash
 
-```bash
-./update-all.sh
-# Username: yenyen
-# Password: token vs-code
-```
+docker login
 
-# Manually
-
-## Build
-
-```
 docker build --pull -t yenyen/vs-code:base base
 docker build -t yenyen/vs-code:gcloud gcloud
 docker build -t yenyen/vs-code:node node
 docker build -t yenyen/vs-code:node-gcloud node-gcloud
 docker build -t yenyen/vs-code:node-gcloud-angular node-gcloud-angular
-```
 
-## Publish
-
-```
-docker login
 docker push yenyen/vs-code:base
 docker push yenyen/vs-code:gcloud
 docker push yenyen/vs-code:node
 docker push yenyen/vs-code:node-gcloud
 docker push yenyen/vs-code:node-gcloud-angular
-```
