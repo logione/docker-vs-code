26c61157aa9d4cf535725b43fcbaa124e2ab81bb
Aurélien Thévoz <aurelien.thevoz@logi.one>
2025-08-25 11:49:55 +0200

    Remove gcloud images and create new angular image (based on node)

--- update-all.sh
+++ update-all.sh
@@ -3,13 +3,9 @@
 docker login
 
 docker build --pull -t yenyen/vs-code:base base
-docker build -t yenyen/vs-code:gcloud gcloud
 docker build -t yenyen/vs-code:node node
-docker build -t yenyen/vs-code:node-gcloud node-gcloud
-docker build -t yenyen/vs-code:node-gcloud-angular node-gcloud-angular
+docker build -t yenyen/vs-code:angular angular
 
 docker push yenyen/vs-code:base
-docker push yenyen/vs-code:gcloud
 docker push yenyen/vs-code:node
-docker push yenyen/vs-code:node-gcloud
-docker push yenyen/vs-code:node-gcloud-angular
+docker push yenyen/vs-code:angular
